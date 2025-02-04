9c8bbc1867c80931dd4ed1e61bf48ced7a4b8b0c
Vaibhav Srivastava <vabnix@hotmail.com>
2025-02-03 20:31:35 -0600

    removing application context, completed testing the creation of beans

--- main/java/com/vaibhav/learning/LearningApplication.java
+++ main/java/com/vaibhav/learning/LearningApplication.java
@@ -2,15 +2,12 @@ package com.vaibhav.learning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class LearningApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext applicationContext = SpringApplication.run(LearningApplication.class, args);
-
-		System.out.println("Checking application Context");
+		SpringApplication.run(LearningApplication.class, args);
 	}
 
 }
